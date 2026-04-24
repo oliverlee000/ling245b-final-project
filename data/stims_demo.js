f9612f7d97a687daf8bff1cfb9fcfbc85ceaf3a4
oliverlee000 <oliver.lee@stanford.edu>
2026-04-23 17:39:16 -0700

    edit

--- data/stims_demo.js
+++ data/stims_demo.js
@@ -1,2 +1,11 @@
-const experimentStimuli = [
+const experimentStimuli = [{
+    "no": "97",
+    "exp_id": "1b_2a",
+    "condition": "a",
+    "item_type": "1",
+    "item_id": "1b_2a-1",
+    "stimulus": "The aide noticed who the generous teacher that the exemplary dean employed graciously and happily gave the prize to.",
+    "question": "Did the teacher give a prize to the student?",
+    "correct_answer": "f"
+  }
 ];
