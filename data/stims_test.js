1fdca03d30dc6cd7784555fb5e125d63524a9de6
oliverlee000 <oliver.lee@stanford.edu>
2026-04-23 17:14:21 -0700

    Created reading_task_2 that uses test stimuli and submits the save data

---
+++ data/stims_test.js
@@ -0,0 +1,32 @@
+const experimentStimuli = [
+  {
+    "no": "97",
+    "exp_id": "1b_2a",
+    "condition": "a",
+    "item_type": "1",
+    "item_id": "1b_2a-1",
+    "stimulus": "The aide noticed who the generous teacher that the exemplary dean employed graciously and happily gave the prize to.",
+    "question": "Did the teacher give a prize to the student?",
+    "correct_answer": "f"
+  },
+  {
+    "no": "98",
+    "exp_id": "1b_2a",
+    "condition": "b",
+    "item_type": "1",
+    "item_id": "1b_2a-1",
+    "stimulus": "The aide noticed how the generous teacher that the exemplary dean employed graciously and happily gave the prize to the student.",
+    "question": "Did the teacher give a prize to the student?",
+    "correct_answer": "f"
+  },
+  {
+    "no": "99",
+    "exp_id": "1b_2a",
+    "condition": "c",
+    "item_type": "1",
+    "item_id": "1b_2a-1",
+    "stimulus": "The aide noticed that the generous teacher that the exemplary dean employed graciously and happily gave the prize to the student.",
+    "question": "Did the teacher give a prize to the student?",
+    "correct_answer": "f"
+  }
+];
