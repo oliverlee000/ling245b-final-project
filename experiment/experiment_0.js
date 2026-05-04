16f50222a6b34db71bd065bcfa976585e7900cd1
oliverlee000 <oliver.lee@stanford.edu>
2026-05-04 09:55:30 -0700

    bug with seed

--- experiment/experiment_0.js
+++ experiment/experiment_0.js
@@ -265,7 +265,6 @@ timeline.push({
 // =========================================================================
 
 // Set seed for psuedorandomization to ensure reproducibility
-seed = 100
 jsPsych.randomization.setSeed("jspsych");
 const shuffledStimuli = jsPsych.randomization.shuffle(experimentStimuli);
 
